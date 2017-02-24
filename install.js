24ba84850fececec96c694d71811ca6e7de4bf1f
Samuel Brekke <brekmister@gmail.com>
2017-02-23 23:09:15 -0600

    1.0.3 release

--- install.js
+++ install.js
@@ -29,44 +29,7 @@ function install() {
       default:
          break;
    }
-}
-
-function runCommands(commands) {
-   var results = [];
-   var error;
-
-   commands.forEach(function(command, index) {
-      print('running \"' + command + "\"...");
-      try {
-         results[index] = cp.execSync(command, {stdio: ['ignore', 'ignore', error]});
-         println('success!');
-      } catch(err) {
-         println('Error\nCommand \"' + command + "\" did not complete successfully...");
-         println("Diagnostics:\n" + err + "\n" + error);
-         println("Exiting...");
-         process.exit(1);
-      }
-   });
-}
-
-function installMac() {
-}
-
-function installWindows() {
-}
-
-function installLinux() {
-   var commands = [
-      'sudo apt-get update',
-      'sudo apt-get install zlib1g-dev ruby ruby-dev build-essential',
-      'npm install -g gulp',
-      'sudo gem install bundler jekyll',
-      'sudo bundler install'
-   ];
-
-   runCommands(commands);
-
-   println('All is well!');
+   
    println(
       '★░░░░░░░░░░░████░░░░░░░░░░░░░░░░░░░░★\n' +
       '★░░░░░░░░░███░██░░░░░░░░░░░░░░░░░░░░★\n' +
@@ -90,4 +53,54 @@ function installLinux() {
       '★░░░░░░░░░█████████████░░░░░░░░░░░░░★\n' +
       '★░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░★'
    );
+}
+
+function runCommands(commands) {
+   var results = [];
+   var error;
+
+   commands.forEach(function(command, index) {
+      print('running \"' + command + "\"...");
+      try {
+         results[index] = cp.execSync(command, {stdio: ['ignore', 'ignore', error]});
+         println('success!');
+      } catch(err) {
+         println('Error\nCommand \"' + command + "\" did not complete successfully...");
+         println("Diagnostics:\n" + err + "\n" + error);
+         println("Exiting...");
+         process.exit(1);
+      }
+   });
+}
+
+function installMac() {
+   var commands = [
+      'sudo npm install -g gulp',
+      'sudo gem install bundler jekyll',
+      'sudo bundler install'
+   ]
+
+   runCommands(commands);
+}
+
+function installWindows() {
+   var commands = [
+      'npm install -g gulp',
+      'gem install bundler jekyll',
+      'bundler install'
+   ]
+
+   runCommands(commands);
+}
+
+function installLinux() {
+   var commands = [
+      'sudo apt-get update',
+      'sudo apt-get install zlib1g-dev ruby ruby-dev build-essential',
+      'npm install -g gulp',
+      'sudo gem install bundler jekyll',
+      'sudo bundler install'
+   ];
+
+   runCommands(commands);
 }
